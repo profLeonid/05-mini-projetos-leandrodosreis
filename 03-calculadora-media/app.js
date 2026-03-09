7f8a5e9a283fd5378bf028deffb343406bb95512
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-03-09 17:02:08 -0300

    exerecicio 3 concluido

---
+++ 03-calculadora-media/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function calcularMedia(){
+    const nota1 = document.getElementById('nota1')
+    const nota2 = document.getElementById('nota2')
+    const nota3 = document.getElementById('nota3')
+    const resultado = document.getElementById('resultado')
+
+    let valor1 = Number(nota1.value)
+    let valor2 = Number(nota2.value)
+    let valor3 = Number(nota3.value)
+    let resultado1 = valor1 + valor2 + valor3 /3
+
+    if(resultado1 >= 7)
+        resultado.textContent = "Aprovado"
+    if(resultado1 > 5 && resultado1 < 6.9)
+        resultado.textContent = "Recuperação"
+    if(resultado1 < 5)
+        resultado.textContent = "Reprovado"
+
+}
