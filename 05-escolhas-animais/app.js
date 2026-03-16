aaff71e7d8b5b855360245ee1dcf9f12aa1b287a
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-03-16 17:01:28 -0300

    logica por tras dos selects

--- 05-escolhas-animais/app.js
+++ 05-escolhas-animais/app.js
@@ -1,9 +1,36 @@
 'use strict'
 
-function vertebrado(){
-    
-}
-
 function definirEscolha(){
+
+    if(valor === "vertebrado"){
+
+        const op1 = document.createElement("option")
+        op1.value = "ave"
+        op1.textContent = "Ave"
+
+        const op2 = document.createElement("option")
+        op2.value = "mamifero"
+        op2.textContent = "Mamífero"
+
+        grupo.appendChild(op1)
+        grupo.appendChild(op2)
+
+    }
+
+    else if(valor === "invertebrado"){
+
+        const op1 = document.createElement("option")
+        op1.value = "inseto"
+        op1.textContent = "Inseto"
+
+        const op2 = document.createElement("option")
+        op2.value = "anelideo"
+        op2.textContent = "Anelídeo"
+
+        grupo.appendChild(op1)
+        grupo.appendChild(op2)
+
+    }
+
 
 }
